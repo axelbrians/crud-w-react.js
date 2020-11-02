8bc252e6b657dca27a7bb8d03cf8ea62ed5f12ef
axel briano <axelbrians@gmail.com>
2020-11-02 12:45:55 +0700

    setup development environtment
    created with webpack babel react.js

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import Navbar from './Navbar';
+import Form from './Form';
+
+class App extends React.Component {
+  render() {
+    return(
+      <div>
+        <Form />
+      </div>
+    );  
+  }
+}
+
+export default App;
